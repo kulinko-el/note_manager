1560829e9539c382b317ea3a584b3cbdfa6476f1
kulinko-el <mamamir123@gmail.com>
2024-12-23 15:57:21 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,16 @@
+#Задание 1
+username = 'kulinko'
+title = 'Новая заметка'
+content = 'Важно'
+status = 'Выполнено'
+created_date = '01.11.24'
+issue_date = '01.01.25'
+
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print('Дата создания заметки:', created_date)
+print('Дедлайн:', issue_date)
+
+
