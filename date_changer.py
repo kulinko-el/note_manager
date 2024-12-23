4098747c6e8432450884de87618170ce8af4fddd
kulinko-el <mamamir123@gmail.com>
2024-12-23 15:58:23 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,14 @@
+#Задание 2
+username = 'kulinko'
+title = 'Новая заметка'
+content = 'Важно'
+status = 'Выполнено'
+created_date = '01.11.24'
+issue_date = '01.01.25'
+
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print('Дата создания заметки:', created_date[0:5])
+print('Дедлайн:', issue_date[0:5])
